1bd230ee702cb1b8d603e0857a7f522a1abe0b75
= <akhilmhdh@gmail.com>
2026-04-22 00:21:23 +0530

    feat: switched to left join for user membership

--- backend/src/services/user/user-dal.ts
+++ backend/src/services/user/user-dal.ts
@@ -91,7 +91,7 @@ export const userDALFactory = (db: TDbClient) => {
           isGhost: false
         })
         .whereIn(`${TableName.Users}.id`, userIds)
-        .join(TableName.UserEncryptionKey, `${TableName.Users}.id`, `${TableName.UserEncryptionKey}.userId`);
+        .leftJoin(TableName.UserEncryptionKey, `${TableName.Users}.id`, `${TableName.UserEncryptionKey}.userId`);
     } catch (error) {
       throw new DatabaseError({ error, name: "Find user enc by user ids batch" });
     }
